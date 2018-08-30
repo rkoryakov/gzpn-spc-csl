5c2fcf73643a8602212e03f14ec3c449f8f54d1e
rkoryakov <rkoryakov@mail.ru>
2018-08-30 14:46:57 +0300

    Created a login controller Added login.html added login.css

--- gzpn-spc-csl/src/main/java/ru/gzpn/spc/csl/configurations/BaseConfiguration.java
+++ gzpn-spc-csl/src/main/java/ru/gzpn/spc/csl/configurations/BaseConfiguration.java
@@ -5,10 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import ru.gzpn.spc.csl.model.BaseEntity;
+import ru.gzpn.spc.csl.model.interfaces.ICProject;
 
 @Configuration
 @EnableJpaRepositories(basePackageClasses = { BaseEntity.class })
-@ComponentScan(basePackageClasses = { BaseEntity.class })
+@ComponentScan(basePackageClasses = { BaseEntity.class, ICProject.class })
 public class BaseConfiguration {
 
 }
---
+++ gzpn-spc-csl/src/main/java/ru/gzpn/spc/csl/services/bl/LoginController.java
@@ -0,0 +1,31 @@
+package ru.gzpn.spc.csl.services.bl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	ServerProperties serverProperties;
+
+	@GetMapping("/login")
+	public String login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout, Model model) {
+
+		if (error != null) {
+			model.addAttribute("error", true);
+		}
+
+		if (logout != null) {
+			model.addAttribute("logout", true);
+		}
+
+		model.addAttribute("contextPath", serverProperties.getServlet().getContextPath() + "/login");
+		return "login";
+	}
+}
