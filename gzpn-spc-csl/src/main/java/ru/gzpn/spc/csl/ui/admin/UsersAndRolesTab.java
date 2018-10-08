82dfd5ac4328579b463067e0802b294b1dd79881
Fisenko.KI <Fisenko.KI@vor02-ws-021.gazprom-neft.local>
2018-10-08 17:52:30 +0300

    Add grid Table

--- gzpn-spc-csl/src/main/java/ru/gzpn/spc/csl/ui/admin/UsersAndRolesTab.java
+++ gzpn-spc-csl/src/main/java/ru/gzpn/spc/csl/ui/admin/UsersAndRolesTab.java
@@ -1,24 +1,25 @@
 package ru.gzpn.spc.csl.ui.admin;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.activiti.engine.IdentityService;
+import org.activiti.engine.identity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.vaadin.data.provider.DataProvider;
 import com.vaadin.data.provider.ListDataProvider;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.NativeSelect;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Notification.Type;
-import com.vaadin.ui.TwinColSelect;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
-
-import ru.gzpn.spc.csl.ui.components.JoinedLayout;
 
 public class UsersAndRolesTab extends VerticalLayout {
 
@@ -26,27 +27,23 @@ public class UsersAndRolesTab extends VerticalLayout {
 
 	private IdentityService identityService;
 
-	private ComboBox<String> searchUserGroup;
+	//private ComboBox<String> searchUserGroup;
+	private TextField searchUserGroup;
+	private NativeSelect<String> selectUserGroup;
 	private HorizontalLayout headerHorizont;
-	private HorizontalLayout bodyHorizontTop;
-	private HorizontalLayout bodyHorizontBottom;
-	private HorizontalLayout bottomHorizont;
+	private VerticalLayout bodyLayout;
 	private VerticalLayout resultPage;
 
 	public UsersAndRolesTab(IdentityService identityService) {
 		this.identityService = identityService;
-		bodyHorizontTop = new HorizontalLayout();
 		headerHorizont = new HorizontalLayout();
-		bodyHorizontBottom = new HorizontalLayout();
-		bottomHorizont = new HorizontalLayout();
+		bodyLayout = dataLayout();
 		resultPage = new VerticalLayout();
 		searchUserGroup = createSearchUserGroup();
+		selectUserGroup = createSelectUserGroup();
 
-		headerHorizont.addComponents(searchUserGroup, addNativeSelect(), addButtonCreate());
-		bodyHorizontTop.addComponents(addJoinLL(), addJoinLR());
-		bodyHorizontBottom.addComponent(addTwinColSelect());
-		bottomHorizont.addComponents(addButtonSave(), addButtonCancel());
-		resultPage.addComponents(headerHorizont, bodyHorizontTop, bodyHorizontBottom, bottomHorizont);
+		headerHorizont.addComponents(searchUserGroup, selectUserGroup, addButtonCreate());
+		resultPage.addComponents(headerHorizont, bodyLayout);
 		refreshData();
 		addComponent(resultPage);
 	}
@@ -58,22 +55,23 @@ public class UsersAndRolesTab extends VerticalLayout {
 		List<String> list = identityService.createUserQuery().list().stream().map(m -> m.getId())
 				.collect(Collectors.toList());
 		ListDataProvider<String> dataProvider = new ListDataProvider<>(list);
-		searchUserGroup.setDataProvider(dataProvider);
+		//searchUserGroup.setDataProvider(dataProvider);
 	}
 
-	private ComboBox<String> createSearchUserGroup() {
-		ComboBox<String> comboBox = new ComboBox<>();
-
-		return comboBox;
+	private TextField createSearchUserGroup() {
+		TextField filterTextField = new TextField();
+		
+		return filterTextField;
 	}
-
-	private NativeSelect<String> addNativeSelect() {
+	
+	private NativeSelect<String> createSelectUserGroup() {
 		List<String> data = new ArrayList<>();
 		data.add("Users");
 		data.add("Roles");
 		NativeSelect<String> natS = new NativeSelect<>(null, data);
 		natS.setEmptySelectionAllowed(false);
 		natS.setSelectedItem(data.get(0));
+		
 		return natS;
 	}
 
@@ -82,37 +80,20 @@ public class UsersAndRolesTab extends VerticalLayout {
 		createButton.addClickListener(event -> Notification.show("The button was clicked", Type.HUMANIZED_MESSAGE));
 		return createButton;
 	}
-
-	private JoinedLayout<Button, ComboBox<String>> addJoinLL() {
-		Button findButtonL = new Button("FindL");
-		ComboBox<String> comboB = new ComboBox<>();
-		JoinedLayout<Button, ComboBox<String>> leftF = new JoinedLayout(comboB, findButtonL);
-		return leftF;
-	}
-
-	private JoinedLayout<Button, ComboBox<String>> addJoinLR() {
-		Button findButtonR = new Button("FindR");
-		ComboBox<String> comboB = new ComboBox<>();
-		JoinedLayout<Button, ComboBox<String>> rightF = new JoinedLayout(comboB, findButtonR);
-		return rightF;
-	}
-
-	private TwinColSelect<String> addTwinColSelect() {
-		List<String> dataD = new ArrayList<>();
-		TwinColSelect<String> twinCol = new TwinColSelect<>(null, dataD);
-		return twinCol;
-	}
-
-	private Button addButtonSave() {
-		Button saveButton = new Button("Save");
-		saveButton.addClickListener(event -> Notification.show("Save user", Type.TRAY_NOTIFICATION));
-		return saveButton;
-	}
-
-	private Button addButtonCancel() {
-		Button cancelButton = new Button("Cancel");
-		cancelButton.addClickListener(event -> Notification.show("Cancel", Type.WARNING_MESSAGE));
-		return cancelButton;
+	
+	private VerticalLayout dataLayout() {
+		VerticalLayout vertL = new VerticalLayout();
+		Collection<User> us = identityService.createUserQuery().list();
+		ListDataProvider<User> dataProvider = DataProvider.ofCollection(us);
+		Grid<User> grid = new Grid<>();
+		grid.addColumn(User :: getId).setCaption("Id");
+		grid.addColumn(User :: getFirstName).setCaption("First Name");
+		grid.addColumn(User :: getLastName).setCaption("Last Name");
+		grid.addColumn(User :: getEmail).setCaption("Email");
+		
+		grid.setDataProvider(dataProvider);
+		vertL.addComponent(grid);
+		return vertL;
 	}
 
 }
