1e8811a28b2a5f18a32f8604a17714c683fa22ef
rkoryakov <rkoryakov@mail.ru>
2018-10-19 18:38:46 +0300

    the document creating layout must be finished!!!

--- gzpn-spc-csl/src/main/java/ru/gzpn/spc/csl/ui/views/CreateDocView.java
+++ gzpn-spc-csl/src/main/java/ru/gzpn/spc/csl/ui/views/CreateDocView.java
@@ -5,6 +5,7 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewBeforeLeaveEvent;
@@ -15,24 +16,28 @@ import com.vaadin.ui.Component;
 import com.vaadin.ui.VerticalLayout;
 
 import ru.gzpn.spc.csl.services.bl.DataProjectService;
+import ru.gzpn.spc.csl.ui.createdoc.DocCreatingLayout;
 
 @SpringView(name = CreateDocView.NAME)
 @UIScope
 public class CreateDocView extends VerticalLayout implements View {
 	public static final String NAME = "createDocView";
 	public static final Logger logger = LoggerFactory.getLogger(CreateDocView.class);
-
+	
 	@Autowired
 	private DataProjectService projectService;
-
+	@Autowired 
+	private MessageSource messageSource;
+	
 	public CreateDocView() {
-		setMargin(true);
+		setMargin(false);
 		setSpacing(true);
 	}
 
 	@PostConstruct
 	void init() {
-
+		DocCreatingLayout layout = new DocCreatingLayout(projectService, messageSource);
+		addComponent(layout);
 	}
 
 	@Override
