1097c6db590ffbc57470915283d54abee44c8a1e
Fisenko.KI <Fisenko.KI@vor02-ws-021.gazprom-neft.local>
2019-01-28 11:39:21 +0300

    add ContractRegisterService

---
+++ gzpn-spc-csl/src/main/java/ru/gzpn/spc/csl/services/bl/ContractRegisterService.java
@@ -0,0 +1,36 @@
+package ru.gzpn.spc.csl.services.bl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+import ru.gzpn.spc.csl.services.bl.interfaces.IContractRegisterService;
+import ru.gzpn.spc.csl.services.bl.interfaces.IContractService;
+import ru.gzpn.spc.csl.services.bl.interfaces.IUserSettigsService;
+
+@Service
+public class ContractRegisterService implements IContractRegisterService {
+
+	@Autowired
+	private IContractService contractService;
+	@Autowired
+	private IUserSettigsService userSettingsService;
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Override
+	public IContractService getContractService() {
+		return contractService;
+	}
+
+	@Override
+	public MessageSource getMessageSource() {
+		return messageSource;
+	}
+
+	@Override
+	public IUserSettigsService getUserSettingsService() {
+		return userSettingsService;
+	}
+
+}
---
+++ gzpn-spc-csl/src/main/java/ru/gzpn/spc/csl/services/bl/interfaces/IContractRegisterService.java
@@ -0,0 +1,6 @@
+package ru.gzpn.spc.csl.services.bl.interfaces;
+
+public interface IContractRegisterService extends IUIService {
+
+	IContractService getContractService();
+}
